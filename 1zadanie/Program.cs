b4c6e9250019b93950fe45ce93bbc6db6b1664ac
«Alexandr» <>
2022-12-16 00:27:44 +0500

    Создали папки и написали первую программу

---
+++ 1zadanie/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите первое  число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+	Console.WriteLine("Самое большое число здесь: " + a);
+}
+else 
+{
+	Console.WriteLine("Самое большое число здесь: " + b);
+}
