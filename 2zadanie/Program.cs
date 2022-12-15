aa47b0ddbd06e9fbaa80924acc029ded4c1b98d1
«Alexandr» <>
2022-12-16 01:52:49 +0500

    Решение третьей задачи

--- 2zadanie/Program.cs
+++ 2zadanie/Program.cs
@@ -10,6 +10,6 @@ int b = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите третье число ");
 int c = Convert.ToInt32(Console.ReadLine());
 int max = a;
-if (b > max) max = a;
+if (b > max) max = b;
 if (c > max) max = c;
 Console.WriteLine("Максимальное число = " + max);
