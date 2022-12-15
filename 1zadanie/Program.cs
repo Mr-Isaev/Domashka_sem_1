6709085c9a2e3330a35e65a623adff3c15dfffab
«Alexandr» <>
2022-12-16 00:35:22 +0500

    Отредактировал первую задачу, добавил исключения

--- 1zadanie/Program.cs
+++ 1zadanie/Program.cs
@@ -1,13 +1,18 @@
 ﻿Console.Clear();
-Console.Write("Введите первое  число");
+Console.Write("Введите первое  число ");
 int a = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число");
+Console.Write("Введите второе число ");
 int b = Convert.ToInt32(Console.ReadLine());
-if (a > b)
-{
-	Console.WriteLine("Самое большое число здесь: " + a);
+if (a == b){
+	Console.WriteLine("Вы написали одинаковые числа");
 }
-else 
-{
-	Console.WriteLine("Самое большое число здесь: " + b);
+else{
+	if (a > b)
+	{
+		Console.WriteLine("Самое большое число здесь: " + a);
+	}
+	else
+	{
+		Console.WriteLine("Самое большое число здесь: " + b);
+	}
 }
