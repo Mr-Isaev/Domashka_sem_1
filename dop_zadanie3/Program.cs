4d9216b509836831ba71fef31fcab62ce915ff57
«Alexandr» <>
2022-12-16 15:25:56 +0500

    Решили дополнительную третью задачу

---
+++ dop_zadanie3/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+Console.Clear();
+Console.WriteLine("Программа принимает условное число B, а после выводит все числа от -B до B ");
+Console.Write("Любое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int a = -b-1;
+while (a < b){
+	a++;
+	Console.WriteLine(a);
+}
